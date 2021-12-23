84b2509389a72b9f8b84069e7246f534e697a1cf
haitaim <76502924+haitaim@users.noreply.github.com>
2021-12-23 17:51:41 -0600

    Fix exercise 1.8

--- chapter1/exercise1_8.rkt
+++ chapter1/exercise1_8.rkt
@@ -7,7 +7,7 @@
   (define (good-enough? guess old-guess)
     (> 0.001 (abs (/ (- old-guess guess) guess))))
   (define (improve guess)
-    (/ (+ (/ x (square guess)) (* 2 guess) 3)))
+    (/ (+ (/ x (square guess)) (* 2 guess)) 3))
   (define (cbrt-iter guess old-guess)
     (if (good-enough? guess old-guess)
         guess
