8a3ea4fddb04e39dc00c2227e43cda3fe493a2f0
haitaim <76502924+haitaim@users.noreply.github.com>
2021-12-28 00:21:10 -0600

    Refactor exercise 1.11
    Increment to n rather than decrement to 0
    Adjust whitespace

--- chapter1/exercise1_11.scm
+++ chapter1/exercise1_11.scm
@@ -11,10 +11,13 @@
 ; c = n - 3
 (define (g n)
   (define (g-iter a b c count)
-    (if (= count 0)
+    (if (= count n)
         a
-        (g-iter (+ a (* 2 b) (* 3 c)) a b (- count 1))))
+        (g-iter (+ a (* 2 b) (* 3 c))
+                a
+                b
+                (+ count 1))))
   (if (< n 3)
       n
-      (g-iter 2 1 0 (- n 2))))
+      (g-iter 2 1 0 2)))
 
