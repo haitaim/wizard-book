84d10d66e9c1d92c53da3f6b49137718e7edf0bd
haitaim <76502924+haitaim@users.noreply.github.com>
2022-01-16 12:30:59 -0600

    Change way to determine sign
    Instead of multiplication, a boolean expression is used.

--- chapter2/section2-1/exercise2-1.scm
+++ chapter2/section2-1/exercise2-1.scm
@@ -1,7 +1,8 @@
 (define (make-rat n d)
   (let ((g (gcd n d))
-        (sign (if (positive? (* n d))
-                             +
-                             -)))
+        (sign (if (and (or (negative? n) (negative? d))
+                       (not (and (negative? n) (negative? d))))
+                  -
+                  +)))
     (cons (sign (abs (/ n g))) (abs (/ d g)))))
 
